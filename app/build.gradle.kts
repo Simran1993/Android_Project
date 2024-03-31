6fbb68b335fcf77bce2daff0d055f226f2b4cb4c
Simran1993 <harsimranjits5102@gmail.com>
2024-03-31 16:04:32 -0400

    Rollback to default

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,22 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+// Room
+    implementation ("androidx.room:room-runtime:2.2.6")
+    annotationProcessor ("androidx.room:room-compiler:2.2.6")
+
+// RecyclerView
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
+// Coroutine for asynchronous tasks
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+
+    // gson
+    implementation(" 'com.squareup.retrofit2:retrofit:2.9.0'")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
