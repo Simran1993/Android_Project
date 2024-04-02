82cdcaabffb4d6b42f3645a9b5180ebac20fe507
Simran1993 <harsimranjits5102@gmail.com>
2024-04-02 11:40:25 -0400

    implement toolbar main interface

--- app/src/main/java/algonquin/cst2335/myapplication/DictionaryMainActivity.java
+++ app/src/main/java/algonquin/cst2335/myapplication/DictionaryMainActivity.java
@@ -6,25 +6,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
-        import androidx.recyclerview.widget.RecyclerView;
-
-        import com.android.volley.Request;
-        import com.android.volley.RequestQueue;
-        import com.android.volley.Response;
-        import com.android.volley.VolleyError;
-        import com.android.volley.toolbox.StringRequest;
-        import com.android.volley.toolbox.Volley;
-        import com.google.gson.Gson;
+import androidx.recyclerview.widget.RecyclerView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
 
 public class DictionaryMainActivity extends AppCompatActivity {
@@ -48,6 +45,9 @@ public class DictionaryMainActivity extends AppCompatActivity {
         rvDefinitions.setLayoutManager(new LinearLayoutManager(this));
         adapter = new DefinitionsAdapter(definitionList);
         rvDefinitions.setAdapter(adapter);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
         String searchTerm = getIntent().getStringExtra("searchTerm");
