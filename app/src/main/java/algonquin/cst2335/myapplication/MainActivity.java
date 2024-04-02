7bc0e1e1098299e781ac378b26412241efb0f197
Nirmal <pate1226@algonquinlive.com>
2024-04-02 18:23:07 -0400

    merge with master

--- app/src/main/java/algonquin/cst2335/myapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myapplication/MainActivity.java
@@ -1,5 +1,10 @@
+package algonquin.cst2335.myapplication;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import algonquin.cst2335.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
