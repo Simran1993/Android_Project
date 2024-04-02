592667c5524067c83935cec2bf518ae84751acb8
Simran1993 <harsimranjits5102@gmail.com>
2024-04-02 10:35:46 -0400

    implement small changes

--- app/src/main/java/algonquin/cst2335/myapplication/SearchEntry.java
+++ app/src/main/java/algonquin/cst2335/myapplication/SearchEntry.java
@@ -15,6 +15,42 @@ public class SearchEntry {
 
     @ColumnInfo(name = "definitions")
     public String definitions; // JSON String of definitions
+
+    private boolean isDescriptionVisible = false;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSearchTerm() {
+        return searchTerm;
+    }
+
+    public void setSearchTerm(String searchTerm) {
+        this.searchTerm = searchTerm;
+    }
+
+    public String getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(String definitions) {
+        this.definitions = definitions;
+    }
+
+    public boolean isDescriptionVisible() {
+        return isDescriptionVisible;
+    }
+
+    public void setDescriptionVisible(boolean descriptionVisible) {
+        isDescriptionVisible = descriptionVisible;
+    }
 }
 
 
